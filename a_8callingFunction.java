2d21293185122b36dbb99822bf9309190ed38250
Shikhar Dutta <149324583+unknownunicorn01@users.noreply.github.com>
2025-09-11 19:21:55 +0530

    Add files via upload

---
+++ a_8callingFunction.java
@@ -0,0 +1,8 @@
+public class a_8callingFunction {
+  public static void Call(){
+    System.out.println("I am another fucntion");
+  }
+  public static void main(String[] args){
+    Call();
+  }
+}
